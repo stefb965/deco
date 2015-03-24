09b5336cfe2d155cc23f428a01f147d4b88a7960
Steven Edouard <steven.edouard1@gmail.com>
2015-03-24 13:13:04 -0700

    syntax errors

--- app/models/blob.js
+++ app/models/blob.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     container: DS.attr('container'),
     name: DS.attr('string'),
-    size: DS.attr('number')
-    lastModified: DS.attr('number')
+    size: DS.attr('number'),
+    lastModified: DS.attr('number'),
     type: DS.attr('string')
 });
