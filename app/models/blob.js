6788446102439a6d0265e93f73be99b360e082c1
Felix Rieseberg <felix@felixrieseberg.com>
2015-03-24 13:03:06 -0700

    Initial Explorer View
    - Simple template with styles
    - Extended blob model

--- app/models/blob.js
+++ app/models/blob.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-    container: DS.attr('container')
+    container: DS.attr('container'),
+    name: DS.attr('string'),
+    size: DS.attr('number')
+    lastModified: DS.attr('number')
+    type: DS.attr('string')
 });
