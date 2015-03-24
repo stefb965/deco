fb9202addde0099c435d142182c8cc88b8d1be1d
Steven Edouard <steven.edouard1@gmail.com>
2015-03-24 13:10:47 -0700

    checkin

--- app/models/account.js
+++ app/models/account.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    key: DS.attr('string')
+    key: DS.attr('string'),
+    activeAccount: DS.attr('boolean')
 });
--- app/models/container.js
+++ app/models/container.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     blobs: function() {
-
+    	return this.store.findAll('blob', { container: this});
     },
     name: DS.attr('string', {defaultValue: ''}),
     publicAccessLevel: ('string', {defaultValue: null})
