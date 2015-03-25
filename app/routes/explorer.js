0a9b6af6c59880b2788b0f996df3f6bf5def3d79
Steven Edouard <steven.edouard1@gmail.com>
2015-03-24 18:06:17 -0700

    do not call blobs.find, we do not implement this because it would literally cause havoc. instead use the container.blobs() function to retrieve blob models

--- app/routes/explorer.js
+++ app/routes/explorer.js
@@ -3,16 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model: function () {
         return Ember.RSVP.hash({
-            containers: this.store.find('container'),
-            blobs: this.store.find('blob')
+            containers: this.store.find('container')
         });
     },
 
     setupController: function (controller, model) {
-        var containers = model.containers,
-            blobs = model.blobs;
+        var containers = model.containers;
 
-        controller.set('cotnainers', containers);
-        controller.set('blobs', blobs);
+        controller.set('containers', containers);
+        
     }
 });
