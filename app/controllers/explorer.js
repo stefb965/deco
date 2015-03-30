25c46f8dd81b7a783b99d9b184a72933b9ad721c
Felix Rieseberg <felix@felixrieseberg.com>
2015-03-30 10:12:33 -0700

    Show blobs
    Closes #19
    - Lists blobs for the containers

--- app/controllers/explorer.js
+++ app/controllers/explorer.js
@@ -1,9 +1,46 @@
 import Ember from 'ember';
 export default Ember.Controller.extend({
     needs: 'application',
+
     activeConnection: Ember.computed.alias('controllers.application.activeConnection'),
-    activeContainer: 0,
-    blobs: function () {
-        return [];
-    }.property('model')
+
+    activeContainer: null,
+
+    blobs: [],
+
+    blobsLoading: true,
+
+    activeContainerObserver: function () {
+        var activeContainer = this.get('activeContainer'),
+            blobs = [],
+            self = this,
+            containerObject;
+
+        this.set('blobsLoading', true);
+
+        if (!activeContainer) {
+            containerObject = this.get('model').get('firstObject');
+            blobs = containerObject.get('blobs');
+
+            this.set('blobs', blobs);
+            this.set('blobsLoading', false);
+        } else {
+            return this.store.find('container', activeContainer).then(function (result) {
+                if (result) {
+                    blobs = result.get('blobs');
+                } else {
+                    blobs = [];
+                }
+
+                self.set('blobs', blobs);
+                self.set('blobsLoading', false);
+            });
+        }
+    }.observes('model', 'activeContainer'),
+
+    actions: {
+        switchActiveContainer: function (selectedContainer) {
+            this.set('activeContainer', selectedContainer);
+        }
+    }
 });
