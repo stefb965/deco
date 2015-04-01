6fcf0ad2ed4acc66c5ef6a33c28939c40e8623bf
Steven Edouard <steven.edouard1@gmail.com>
2015-03-31 19:29:03 -0700

    implement blob.link() on model to obtain link to content

--- app/models/blob.js
+++ app/models/blob.js
@@ -25,6 +25,37 @@ export default DS.Model.extend({
     lastModified: DS.attr('date'),
     container_id: DS.attr('string'),
     type: DS.attr('string'),
+    link: function () {
+        var self = this;
+        return new Ember.RSVP.Promise(function (resolve){
+                accountUtil.getActiveAccount(self.store).then(function (account) {
+                var azureStorage = window.requireNode('azure-storage');
+                var blobService = azureStorage.createBlobService(account.
+                    get('name'),
+                    account.get('key'));
+                var startDate = new Date();
+                var expiryDate = new Date(startDate);
+                // set the link expiration to 200 minutes in the future.
+                expiryDate.setMinutes(startDate.getMinutes() + 200);
+                startDate.setMinutes(startDate.getMinutes() - 100);
+
+                var sharedAccessPolicy = {
+                    AccessPolicy: {
+                      Permissions: azureStorage.BlobUtilities.SharedAccessPermissions.READ,
+                      Start: startDate,
+                      Expiry: expiryDate
+                  }
+                };
+
+                var token = blobService.generateSharedAccessSignature(self.get('container_id'), self.get('name'), sharedAccessPolicy);
+
+                // generate a url in which the user can have access to the blob
+                var sasUrl = blobService.getUrl(self.get('container_id'), self.get('name'), token);
+
+                return Ember.run(null, resolve, sasUrl);
+            });
+        });
+    }.property(),
     // returns a memory stream of the blob
     toStream: function () {
         var MemoryStream = window.requireNode('memorystream');
