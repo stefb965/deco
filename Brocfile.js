89a6456f991dadcfce6182d35fd8ddb57f76b493
Steven Edouard <steven.edouard1@gmail.com>
2015-04-01 18:15:46 -0700

    implement custom testloader for testem. modify test index.tml to intercept log output. command line output comes from nw and is cleaned up by nw-runner. nw-runner also downloads nw into your local repo if it is not already there

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,6 @@
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
-
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
@@ -16,4 +15,11 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+// nwjs requires a package.json in order to runu anything
+var tree = new Funnel('tests', {
+    files: ['package.json'],
+    destDir: 'tests'
+});
+module.exports = mergeTrees([app.toTree(), tree]);
