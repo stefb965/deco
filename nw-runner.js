9bf345a2ee059b0e5ed5e13260db5a0d9b43f0e3
Steven Edouard <steven.edouard1@gmail.com>
2015-04-08 13:58:05 -0700

    use the nw that comes in the nw package instead of downloading it on the fly

--- nw-runner.js
+++ nw-runner.js
@@ -2,55 +2,10 @@ var extractZip = require('extract-zip');
 var Download = require('download');
 var exec = require('child_process').exec;
 var NW_VERSION = '0.12.0';
-var osx_64 = './bin/nw/nwjs-v' + NW_VERSION + '-osx-x64/nwjs.app/Contents/MacOS/nwjs';
-var linux_64 = './bin/nw/nwjs-v' + NW_VERSION + '-linux-x64/nw';
 var os = require('os');
 var fs = require('fs');
 
-
-// we'll assume just 64 bit
-if(os.arch() != 'x64'){
-    throw 'we only support running tests on 64 bit machines'
-}
-
-if(os.platform() === 'darwin'){
-    if(!fs.existsSync(osx_64)) {
-        new Download({mode: '755', extract: true})
-        .get('http://dl.nwjs.io/v0.12.0/nwjs-v' + NW_VERSION + '-osx-x64.zip')
-        .dest('./bin/nw')
-        .run(function (err, files) {
-            if(err){
-                return console.error(err);
-            }
-            runNw(osx_64);
-        });
-    }
-    else{
-        runNw(osx_64);
-    }
-
-}
-else if(os.platform() === 'linux'){
-    if(!fs.existsSync(linux_64)) {
-        console.log('downloading nw.js for linux to ' + linux_64);
-        new Download({mode: '755', extract: true})
-        .get('http://dl.nwjs.io/v0.12.0/nwjs-v' + NW_VERSION + '-linux-x64.tar.gz')
-        .dest('./bin/nw')
-        .run(function (err, files) {
-            if(err){
-                return console.error(err);
-            }
-            console.log('sucessfully extracted linux nw.js ');
-            runNw(linux_64);
-        });
-    }
-    else{
-        runNw(linux_64);
-    }
-}
-else{
-    throw 'we only support running tests on mac and linux for now'
-}
+runNw('./node_modules/nw/bin/nw');
 
 function runNw(nwPath){
     var nw;
