bdd528f1572223456df9715b27d567ad7abb7723
Felix Rieseberg <felix@felixrieseberg.com>
2015-04-10 13:34:12 -0700

    Working Uploads
    Uploads!

--- app/controllers/blob.js
+++ app/controllers/blob.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 import filesize from '../utilities/filesize.js';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
     prettySize: function () {
-        var size = this.get('size');
+        var size = this.get('model.size');
         return filesize(parseInt(size)).human('si');
-    }.property('size')
+    }.property('model.size')
 });
--- app/controllers/explorer.js
+++ app/controllers/explorer.js
@@ -26,6 +26,8 @@ export default Ember.Controller.extend({
 
             this.set('blobs', blobs);
             this.set('blobsLoading', false);
+
+            Ember.run.next(() => this.set('activeContainer', containerObject.id));
         } else {
             return this.store.find('container', activeContainer).then(function (result) {
                 if (result) {
@@ -45,11 +47,30 @@ export default Ember.Controller.extend({
             this.set('activeContainer', selectedContainer);
         },
 
+        uploadBlob: function () {
+            var nwInput = Ember.$('#nwUploadFile'),
+                activeContainer = this.get('activeContainer'),
+                self = this;
+
+            nwInput.change(function () {
+                self.store.find('container', activeContainer).then(foundContainer => {
+                    foundContainer.uploadBlob(this.value).then(function (result) {
+                        self.send('refreshBlobs');
+                        console.log(result);
+                    }, function (error) {
+                        toast(error, 4000);
+                    });
+                });
+                nwInput.off('change');
+            });
+
+            nwInput.click();
+        },
+
         downloadBlob: function (blob, name) {
             var nwInput = Ember.$('#nwSaveInput');
 
             nwInput.attr('nwsaveas', name);
-
             nwInput.change(function () {
                 blob.toFile(this.value);
                 nwInput.off('change');
@@ -60,6 +81,22 @@ export default Ember.Controller.extend({
 
         selectBlob: function(blob) {
             this.set('selectedBlob', blob);
+        },
+
+        refreshBlobs: function () {
+            var blobs = [],
+                self = this;
+
+            this.store.find('container', this.get('activeContainer')).then(function (result) {
+                if (result) {
+                    blobs = result.get('blobs');
+                } else {
+                    blobs = [];
+                }
+
+                self.set('blobs', blobs);
+                self.set('blobsLoading', false);
+            });
         }
     }
 });
