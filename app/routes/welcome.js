5c927948c2b27620b10b45bcf63302da82419b5f
Felix Rieseberg <felix@felixrieseberg.com>
2015-04-16 03:05:01 +0400

    Style Fix

--- app/routes/welcome.js
+++ app/routes/welcome.js
@@ -4,10 +4,10 @@ export default Ember.Route.extend({
     model: function () {
         return this.store.find('account');
     },
-    
+
     actions: {
         selectize: function () {
-            Ember.run.scheduleOnce('afterRender', this, function() {
+            Ember.run.scheduleOnce('afterRender', this, function () {
                 Ember.$('select').material_select();
             });
         }
