650634a9a4d7f36494fe266175af03bb4b11dbc5
Felix Rieseberg <felix@felixrieseberg.com>
2015-06-08 07:30:39 -0700

    Node-Webkit Builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
                     max_preserve_newlines: 1
                 },
                 files: {
-                  'application_files': ['app/**/*.js']
+                    'application_files': ['app/**/*.js']
                 }
             }
         },
@@ -30,12 +30,31 @@ module.exports = function (grunt) {
                 config: '.jscsrc',
                 esnext: true
             }
+        },
+        // Execute individual builds with the commands below
+        // Warning: If trying to compile Windows on OS X / Linux,
+        // you'll need Wine.
+        // 
+        // grunt compile --platforms:'win'
+        // grunt compile --platforms:'osx'
+        // grunt compile --platforms:'linux'
+        nodewebkit: {
+            options: {
+                platforms: ['win', 'osx', 'linux'],
+                buildDir: './webkitbuilds',
+                macIcns: './public/icon/ase.icns',
+                winIco: './public/icon/ase.ico'
+            },
+            src: ['./dist/'] // Your node-webkit app
         }
     });
 
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
     grunt.loadNpmTasks('grunt-js-beautify');
+
     grunt.registerTask('codestyle', ['jshint', 'jscs']);
     grunt.registerTask('test', ['codestyle']);
     grunt.registerTask('default', ['test']);
     grunt.registerTask('beautify', ['js_beautify']);
+    grunt.registerTask('compile', ['ember build --production', 'nodewebkit ']);
 };
