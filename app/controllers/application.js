434f47d7e9db83166b0a55d446a142f380956e92
Felix Rieseberg <felix@felixrieseberg.com>
2015-07-01 12:13:29 -0700

    Correct Height for Container List
    Closes #104

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+
 export default Ember.Controller.extend({
     fs: Ember.computed.alias('nodeServices.fs'),
     nodeServices: Ember.inject.service(),
     activeConnection: null,
     versionNumber: function () {
         var fs = this.get('fs');
-        return fs.existsSync('version') ? fs.readFileSync('version', { encoding:'utf8' }) : 'unknown';
+        return fs.existsSync('version') ? fs.readFileSync('version', {encoding:'utf8'}) : 'unknown';
     }.property()
 });
