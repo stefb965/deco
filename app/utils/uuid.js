f3af4f14877e766e2e79ad3d0f48fab45a49a78b
Felix Rieseberg <felix@felixrieseberg.com>
2015-07-04 19:16:44 -0700

    UUID Function

---
+++ app/utils/uuid.js
@@ -0,0 +1,10 @@
+export default {
+    _s4: function () {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    },
+
+    makeUUID: function () {
+        var s4 = this._s4;
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
+};
