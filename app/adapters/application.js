eb4b1e11f9db2a3e3635c842e3d2914d9149574c
Felix Rieseberg <felix@felixrieseberg.com>
2015-07-11 17:04:55 -0700

    Remove App Stuff

--- app/adapters/application.js
+++
@@ -1,6 +0,0 @@
-import App from '../app';
-import DS from 'ember-data';
-App.ApplicationSerializer = DS.LSSerializer.extend();
-export default DS.LSAdapter.extend({
-    namespace: 'azureexplorer'
-});
--- app/controllers/blob.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import filesize from '../utilities/filesize.js';
-
-export default Ember.Controller.extend({
-    prettySize: function () {
-        var size = this.get('model.size');
-        return filesize(parseInt(size)).human('si');
-    }.property('model.size')
-});
