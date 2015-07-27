b585b91bf47dba5dcd212c0e9e53fed4af60697d
Steven Edouard <sedouard@microsoft.com>
2015-07-27 15:22:13 -0400

    feat(models/blob) - display lock status

--- app/controllers/blob.js
+++ app/controllers/blob.js
@@ -12,5 +12,9 @@ export default Ember.Controller.extend({
     prettySize: function () {
         var size = this.get('model.size');
         return filesize(size).human('si');
-    }.property('model.size')
+    }.property('model.size'),
+
+    isLocked: function () {
+        return this.get('model.leaseState') !== 'available' || this.get('model.leaseStatus') === 'locked';
+    }.property('model.leaseState', 'model.leaseStatus')
 });
