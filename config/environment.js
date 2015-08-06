7ec98e005f53564851c2d19144e2eed833cd90ef
Felix Rieseberg <felix@felixrieseberg.com>
2015-08-06 11:06:50 -0700

    Ensure EMBER_CLI_NW
    - `ember-cli-nwjs` is looking for EMBER_CLI_NW on `process.env`. We’re
    just adding it in configuration now, ensuring that it’s always true.
    - Also fixed tab size for configuration.

--- config/environment.js
+++ config/environment.js
@@ -1,65 +1,70 @@
 /* jshint node: true */
 
-module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'azureexplorer',
-    environment: environment,
-    baseURL: '/',
-    locationType: 'hash',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
-    
-    exportApplicationGlobal: true,
+module.exports = function (environment) {
+    var ENV = {
+        modulePrefix: 'azureexplorer',
+        environment: environment,
+        baseURL: '/',
+        locationType: 'hash',
+        EmberENV: {
+            FEATURES: {
+                // Here you can enable experimental features on an ember canary build
+                // e.g. 'with-controller': true
+            }
+        },
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    },
-    
-    sassOptions: {
-      includePaths: ['bower_components/materialize/sass']
-    },
+        exportApplicationGlobal: true,
 
-    /*jshint -W109 */
-    contentSecurityPolicy: {
-      'default-src': "'none'",
-      'script-src': "'unsafe-inline' 'self' https://cdn.mxpnl.com", // Allow scripts from https://cdn.mxpnl.com
-      'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
-      'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
-      'img-src': "'self'",
-      'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com 
-      'media-src': "'self'"
+        APP: {
+            // Here you can pass flags/options to your application instance
+            // when it is created
+        },
+
+        sassOptions: {
+            includePaths: ['bower_components/materialize/sass']
+        },
+
+        /*jshint -W109 */
+        contentSecurityPolicy: {
+            'default-src': "'none'",
+            'script-src': "'unsafe-inline' 'self' https://cdn.mxpnl.com", // Allow scripts from https://cdn.mxpnl.com
+            'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+            'connect-src': "'self' https://api.mixpanel.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+            'img-src': "'self'",
+            'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com 
+            'media-src': "'self'"
+        }
+        /*jshint +W109 */
+    };
+
+    if (environment === 'test') {
+        // Testem prefers this...
+        ENV.baseURL = '/';
+        ENV.locationType = 'none';
+
+        // keep test console output quieter
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+        ENV.APP.rootElement = '#ember-testing';
     }
-    /*jshint +W109 */
-  };
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    if (process && process.env) {
+        process.env.EMBER_CLI_NW = true;
+    }
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+    /*
+      if (environment === 'production') {
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+      }
 
-/*
-  if (environment === 'production') {
-  }
-
-  if (environment === 'development') {
-    ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
-    ENV.APP.LOG_TRANSITIONS = true;
-    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-*/
-  return ENV;
+      if (environment === 'development') {
+        ENV.APP.LOG_RESOLVER = true;
+        ENV.APP.LOG_ACTIVE_GENERATION = true;
+        ENV.APP.LOG_TRANSITIONS = true;
+        ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        ENV.APP.LOG_VIEW_LOOKUPS = true;
+      }
+    */
+    return ENV;
 };
