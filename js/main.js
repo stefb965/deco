4559a9c10fa11c5ab02234b6b3024d551e0aaa9c
Miguel Caballero Pinto <miguelca@microsoft.com>
2016-02-03 12:09:27 -0800

    Clean index.html and added link to release notes.

--- js/main.js
+++ js/main.js
@@ -30,46 +30,6 @@ function init() {
             $('#osxDownload').show();
             $('#windowsDonwload').show();                                  
     }
-    
-    $('#features').click(function () {
-        var features = $('#featuresList'),
-            opener = $('#featuresOpener');
-
-        if (features.is(':hidden')) {
-            features.slideDown();
-            opener.slideUp();
-        } else {
-            features.slideUp();
-            opener.slideDown();
-        }
-    });
-
-    $('#issues').click(function () {
-        var issues = $('#issuesList'),
-            opener = $('#issuesOpener');
-
-        if (issues.is(':hidden')) {
-            issues.slideDown();
-            opener.slideUp();
-        } else {
-            issues.slideUp();
-            opener.slideDown();
-        }
-    });
-   
-   $('#fixes').click(function () {
-        var fixes = $('#fixesList'),
-            opener = $('#fixesOpener');
-
-        if (fixes.is(':hidden')) {
-            fixes.slideDown();
-            opener.slideUp();
-        } else {
-            fixes.slideUp();
-            opener.slideDown();
-        }
-    }); 
-    
 };
 
 init();
