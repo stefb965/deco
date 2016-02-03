db8c8145fb7a660c0efdb1834f12e8ea7568cdbb
Miguel Caballero Pinto <miguelca@microsoft.com>
2016-02-03 11:56:35 -0800

    Show download buttons according to OS.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-function isLinux() {
-    return (navigator.platform.indexOf('Linux') > -1 || navigator.platform.indexOf('X11') > -1);
+function getOS() {
+    var os = '';
+    
+    if(navigator.platform.indexOf('Linux') > -1 || navigator.platform.indexOf('X11') > -1) {
+        os = 'linux';
+    } else if(navigator.platform.indexOf('Mac') > -1) {
+        os = 'osx';
+    } else if(navigator.platform.indexOf('Win') > -1) {
+        os = 'windows';
+    }
+    
+    return os;
 }
 
 function init() {
+    var os = getOS();
+    
+    switch(os) {
+        case 'linux':
+            $('#linuxDownload').show();
+            break;
+        case 'osx':
+            $('#osxDownload').show();
+            break;
+        case 'windows':
+            $('#windowsDonwload').show();
+            break;
+        default: 
+            $('#linuxDownload').show();
+            $('#osxDownload').show();
+            $('#windowsDonwload').show();                                  
+    }
+    
     $('#features').click(function () {
         var features = $('#featuresList'),
             opener = $('#featuresOpener');
